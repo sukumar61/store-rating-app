9670cc6f18ffc2e75b64d3a891ff5f95a8eb0eaf
SukumarRenukuntla <sukumarrenukuntla@gmail.com>
2026-05-31 16:23:15 +0530

    feat: added JWT authorization middleware and stores table

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwttoken from "jsonwebtoken"
+export const authenticateUser=(req,res,next)=>{
+    try{
+         const header=req.headers.authorization
+         if(!header){
+            return res.send(401).json({success:false,message:"Access Denied"})
+         }
+        const token =header.split(" ")[1]
+        const verifyToken=jwttoken.verify(token,process.env.JWT_SECRET_TOKEN)
+        req.user=verifyToken
+        next()
+    }
+    catch(e){
+        console.log(e.message)
+        return(res.status(401).json({message:"Invalid Token"}))
+    }
+
+    next()
+}
--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,15 @@ import express from "express"
 import pool from "./config/db.js"
 import userRoutes from "./routes/userRoutes.js"
 import authRouters from "./routes/authRoutes.js"
+import {authenticateUser} from "./middleware/authMiddleware.js"
 const app=express()
 app.use(express.json())
 app.listen(3000,()=>{
     console.log("listing request")
 })
+app.post("/profile",authenticateUser,(req,res)=>{
+    res.json({message:req.user})
+})
 app.use("/auth",authRouters)
 app.use("/users",userRoutes)
 
-app.get("/",(req,res)=>{
-    res.send("server working")
-
-})
